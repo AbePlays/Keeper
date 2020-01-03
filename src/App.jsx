f861a548874e786a423b2276fd885358fbffddf9
Abe <abhi.rawat456@gmail.com>
2020-01-03 19:22:22 +0530

    Adding Dynamic Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Heading from "./Header";
 import Footer from "./Footer";
-import NoteGen from "./NoteGen";
+import Note from "./Note";
+import NotesData from "./notes";
+
+function genNotes(note) {
+  return <Note key={NotesData.key} title={note.title} body={note.content} />;
+}
 
 function App() {
   return (
     <div>
       <Heading />
-      <NoteGen />
+      {NotesData.map(genNotes)}
       <Footer />
     </div>
   );
