cebef7c687b3922d71369df571adddede62419a7
Abe <abhi.rawat456@gmail.com>
2020-01-04 16:52:20 +0530

    Adding Notes Creation Functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  function addNote(note) {
+    setNotes(prevNotes => {
+      return [...prevNotes, note];
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+      {notes.map(note => {
+        return <Note title={note.title} content={note.content} />;
+      })}
       <Footer />
     </div>
   );
